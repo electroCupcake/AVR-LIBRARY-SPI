97ed26c7af7769b515abffd686b9503a4f08e8fe
John Convertino <electrobs@gmail.com>
2017-08-31 18:51:51 -0400

    initial Commit

---
+++ spi.c
@@ -0,0 +1,96 @@
+/*
+ * spi.c
+ *
+ *  Created on: Oct 20, 2015
+ *      Author: jconvertino
+ * 
+    Copyright (C) 2015 John Convertino
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <avr/io.h>
+#include <avr/common.h>
+#include <avr/interrupt.h>
+
+#include "spi.h"
+
+void spiInit(uint8_t mode, uint8_t clkRate, uint8_t clkDouble)
+{
+	SPCR = 0;
+
+	switch(mode)
+	{
+		case MASTER_MODE:
+			SPCR = clkRate;
+			SPSR = clkDouble;
+			DDRB |= (1 << DDB5) | (1 << DDB3) | (1 << DDB2); //output SCK, MOSI, SS
+			DDRB &= ~(1 << DDB4); //input MISO
+			SPCR |= (1 << MSTR) | (1 << SPE);
+			break;
+		case SLAVE_MODE:
+		default:
+			DDRB |= (1 << DDB4); //output on MISO
+			DDRB &= ~(1 << DDB5) & ~(1 << DDB3) & ~(1 << DDB2); //input MOSI, SCK, SS
+			SPCR |= (1 << SPE);
+			break;
+	}
+}
+
+void spiSettings(uint8_t mode, uint8_t endian)
+{
+	SPCR |= (mode << SPI_MODE_LOC) | (endian << DORD);
+	SPCR &= SPI_MODE_MSK | (mode << SPI_MODE_LOC) | (endian << DORD);
+}
+
+uint8_t spiTrans(uint8_t data)
+{
+	uint8_t tmp_SREG = SREG;
+	cli();
+
+	SPDR = data;
+
+	//better idea needed to deal with collisions
+	if(SPSR & (1 << WCOL))
+	{
+		SREG = tmp_SREG;
+		return data;
+	}
+
+	while((SPSR & (1 << SPIF)) == 0);
+
+	SREG = tmp_SREG;
+	return SPDR;
+}
+
+void slaveLow(volatile uint8_t *pinPort, uint8_t pinNum)
+{
+	*pinPort &= ~(1 << pinNum);
+}
+
+void slaveHigh(volatile uint8_t *pinPort, uint8_t pinNum)
+{
+	*pinPort |= (1 << pinNum);
+}
+
+void spiIsr_Enabled()
+{
+	SPCR |= (1 << SPIE);
+}
+
+void spiIsr_Disabled()
+{
+	SPCR &= ~(1 << SPIE);
+}
---
+++ spi.h
@@ -0,0 +1,75 @@
+/*
+ * spi.h
+ *
+ *  Created on: Oct 20, 2015
+ *      Author: jconvertino
+ * 
+    
+    Copyright (C) 2015 John Convertino
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include <inttypes.h>
+
+//modes
+#define SLAVE_MODE  0
+#define MASTER_MODE 1
+#define SPI_MODE_0	0 //sample rising, setup falling, clk normally low
+#define SPI_MODE_1  1 //sample falling, setup rising, clk normally low
+#define SPI_MODE_2	2 //sample falling, setup rising, clk normally high
+#define SPI_MODE_3  3 //sample rising, setup falling, clk normally high
+#define MSB_FIRST   0
+#define LSB_FIRST   1
+
+//shift value
+#define SPI_MODE_LOC CPHA
+#define SPI_MODE_MSK 0xD3
+
+//SCK dividers
+/* LVL_0 = F_CPU/4
+ * LVL_1 = F_CPU/16
+ * LVL_2 = F_CPU/64
+ * LVL_4 = F_CPU/128
+ *
+ * The above is all at NORMAL_SPD, setting to DOUBLE_SPD will
+ * cut each of the divisors in half (aka F_CPU/4 is now: F_CPU/2).
+ */
+#define FOSC_LVL_0  0
+#define FOSC_LVL_1  1
+#define FOSC_LVL_2  2
+#define FOSC_LVL_3 	3
+#define NORMAL_SPD	0
+#define DOUBLE_SPD 	1
+
+//will default to MSB first, ISR interrupt disabled, and SPI_MODE_0
+void spiInit(uint8_t mode, uint8_t clkRate, uint8_t clkDouble);
+
+void spiSettings(uint8_t mode, uint8_t endian);
+
+uint8_t spiTrans(uint8_t data);
+
+void slaveLow(volatile uint8_t *pinPort, uint8_t pinNum);
+
+void slaveHigh(volatile uint8_t *pinPort, uint8_t pinNum);
+
+void spiIsr_Enabled();
+
+void spiIsr_Disabled();
+
+#endif /* SPI_H_ */
